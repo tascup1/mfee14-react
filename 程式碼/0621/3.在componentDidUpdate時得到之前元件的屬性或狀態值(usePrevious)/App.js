06c4b942b9123eca75bc1d2bc7b4ee8d7c546f71
Eddy Chang <eddy@joomla.com.tw>
2021-06-21 11:06:27 +0800

    Update App.js

--- 程式碼/0621/3.在componentDidUpdate時得到之前元件的屬性或狀態值(usePrevious)/App.js
+++ 程式碼/0621/3.在componentDidUpdate時得到之前元件的屬性或狀態值(usePrevious)/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
+// 自訂的勾子
+// https://zh-hant.reactjs.org/docs/hooks-faq.html#how-to-get-the-previous-props-or-state
 function usePrevious(value) {
   const ref = useRef()
   useEffect(() => {
@@ -10,8 +12,11 @@ function usePrevious(value) {
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // 這裡可以得到之前的屬性或狀態
   const prevCount = usePrevious(count)
 
+  // 在useEffect裡使用
   useEffect(() => {
     console.log(count, prevCount)
   }, [count, prevCount])
