8bca46f17dd978e96a0ae71c3fe8613bff4cb259
Eddy Chang <eddy@joomla.com.tw>
2021-06-21 09:43:48 +0800

    Create App.js

---
+++ 程式碼/0621/1.函式型元件-componentDidMount-componentDidUpdate/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+
+function App() {
+  const [total, setTotal] = useState(0)
+
+  // 指示目前目前元件進入更新狀態
+  const [start, setStart] = useState(false)
+
+  // componentDidMount
+  // 元件初次(一開始)呈現在網頁上
+  useEffect(() => {
+    console.log('componentDidMount ')
+    setStart(true)
+  }, [])
+
+  // componentDidUpdate+componentDidMount
+  // 第一次還是會執行裡面的程式碼
+  useEffect(() => {
+    console.log(
+      'componentDidUpdate+componentDidMount ',
+      total
+    )
+  }, [total])
+
+  // componentDidUpdate
+  useEffect(() => {
+    // ignore略過
+    if (!start) return
+
+    console.log('componentDidUpdate ', total)
+  }, [total])
+
+  return (
+    <>
+      <h1>{total}</h1>
+      <button
+        onClick={() => {
+          setTotal(total + 1)
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          setTotal(total - 1)
+        }}
+      >
+        -1
+      </button>
+    </>
+  )
+}
+
+export default App
