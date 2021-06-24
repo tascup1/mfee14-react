155d9d091e2642d3e24eaffcf16c9d19d07c9dc8
Eddy Chang <eddy@joomla.com.tw>
2021-06-24 10:10:50 +0800

    ad

---
+++ 程式碼/0624/1.router-使用說明-1/src/pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login(props) {
+  return (
+    <>
+      <h1>Login</h1>
+      <h2>
+        會員登入情況: {props.auth ? '已經登入' : '未登入'}
+      </h2>
+      <button onClick={() => props.setAuth(!props.auth)}>
+        {props.auth ? '登出' : '請登入'}
+      </button>
+    </>
+  )
+}
+
+export default Login
